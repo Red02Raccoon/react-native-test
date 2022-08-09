4b4f7d82185d25f922b73211192bcb05b68aafee
Red02Raccoon <red02raccoon@gmail.com>
2022-08-09 23:39:45 +0300

    feature: choose color

--- App.js
+++ App.js
@@ -1,32 +1,94 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+
+const colors = [
+  { color: '#2aa198' },
+  { color: '#268bd2' },
+  { color: '#d33682' },
+  { color: '#cb4b16' },
+];
 
 export default function App() {
+  const [currentColor, setCurrentColor] = useState(null);
+
+  const handleColorChange = (color) => () => {
+    setCurrentColor(color);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! ohohoh</Text>
+    <SafeAreaView
+      style={[
+        styles.container,
+        styles.color,
+        currentColor && { backgroundColor: currentColor },
+      ]}
+    >
+      <View style={styles.inner}>
+        <Text>Choose your color 🎉🎉🎉</Text>
+
+        <View style={styles.box}>
+          {colors.map(({ color }) => (
+            <TouchableOpacity
+              style={[styles.boxItem, { backgroundColor: color }]}
+              key={color}
+              onPress={handleColorChange(color)}
+            >
+              <Text>{color}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
       <StatusBar style="auto" />
-      <Text>Hello world! 🎉🎉🎉</Text>
-      <Text>{Platform.OS}</Text>
-      <Text>{Platform.Version}</Text>
-      <Text>Open up App.js to start working on your app! ohohoh</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  color: {
     ...Platform.select({
       android: {
         backgroundColor: 'green',
       },
       default: {
-        backgroundColor: 'blue',
+        backgroundColor: 'pink',
       },
     }),
+  },
+  inner: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: 'white',
+  },
+  box: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 10,
+  },
+  boxItem: {
+    width: 65,
+    height: 65,
+    marginHorizontal: 3,
+    marginVertical: 3,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    textAlign: 'center',
   },
 });
